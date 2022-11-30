67b902d269ca4300ee93036449d6ab8b190aaa79
RevanRehimli313 <revanrehimli02@gmail.com>
2022-11-30 08:39:33 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+// 1. logGreeting() adında funksiya tərtib edin hansıki
+//  konsola «[Adınız] is a JS developer» mesajı çıxarsın.
+
+
+    let name = prompt('adinizi daxil edin: ')
+    console.log(` ${name}  is a JS developer `)
+

e77e5b1c080f592677bb3b6d97ec27d8a181a7e0
Ali-GreenHeart <56336368+Ali-GreenHeart@users.noreply.github.com>
2022-11-30 10:58:00 +0400

    done ✅

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // 1. logGreeting() adında funksiya tərtib edin hansıki
 //  konsola «[Adınız] is a JS developer» mesajı çıxarsın.
 
+// A: done ✅
 
     let name = prompt('adinizi daxil edin: ')
     console.log(` ${name}  is a JS developer `)
